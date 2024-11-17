961219cf84d80a1d4f6e327072da93b8e72a17f4
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2024-11-17 23:59:57 +0400

    ENH: added simple test to check probabilities. To be remove later.

---
+++ mabwiser/mytest.py
@@ -0,0 +1,26 @@
+from mabwiser.mab import MAB, LearningPolicy, NeighborhoodPolicy
+arms = ['Arm1', 'Arm2']
+decisions = ['Arm1', 'Arm1', 'Arm2', 'Arm1']
+rewards = [20, 17, 25, 9]
+contexts = [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.1, 1.1]]
+mab = MAB(arms, LearningPolicy.EpsilonGreedy(epsilon=0.1))
+# mab = MAB(arms, LearningPolicy.UCB1())
+# mab.fit(decisions, rewards, contexts=contexts)
+mabs = [
+    MAB(arms, LearningPolicy.Random()),
+    MAB(arms, LearningPolicy.Popularity()),    
+    MAB(arms, LearningPolicy.EpsilonGreedy(epsilon=0.1)),
+    MAB(arms, LearningPolicy.UCB1()),
+    MAB(arms, LearningPolicy.LinGreedy(epsilon=0.3)),
+    MAB(arms, LearningPolicy.LinUCB(alpha=0.2)),
+    MAB(arms, LearningPolicy.Softmax(tau=10.0)),
+    # MAB(arms, LearningPolicy.ThompsonSampling(...)), # TODO, maybe, because it's binary
+]
+for mab in mabs:
+    print(mab.learning_policy)
+    mab.fit(decisions, rewards, contexts=contexts if mab.is_contextual else None)
+    print(f"mab.predict_expectations(): {mab.predict_expectations(contexts)}")
+    for _ in range(10):
+        print(f"mab.predict(): {mab.predict(contexts)}")
+
+    print("")
